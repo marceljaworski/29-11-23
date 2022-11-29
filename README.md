5e8eaefdf74a6acef06c9fa37ab16e768fb7bc27
Marcelearn <jaworskimarcel@gmail.com>
2022-11-29 15:12:40 +0100

    express server

---
+++ README.md
@@ -0,0 +1,4 @@
+Erstelle einen Server, der folgende API bereitstellt:
+1. Ein Endpoint soll alle User aus der angehängten Datei zurückgeben.
+2. Ein weiterer Endpoint soll einen einzigen User anhand der ID ausgeben. Sollte die ID nicht gefunden werden, muss ein passender Statuscode zurückgeliefert werden.
+3. Der Endpoint aus Aufgabe 1 soll so erweitert werden, dass optional(!) nach dem Namen gesucht werden kann.
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// import express from "express";
+const users = require('./users.json');
+// import users from './users.json' assert {type: "json"};
+
+const server = express();
+
+server.listen(3000)
+
+server.get("/users", (request, response) => {
+    const userName = users.find(user => user.first_name === request.query.name)
+    if (userName) {
+        return response.json(userName)
+    }
+    response.json(users)
+})
+server.get("/users/:id", (request, response) => {
+    const { id } = request.params;
+    const user = users.find(user => {
+        return user.id === +id
+    })
+    if(isNaN(id)){
+        return response.send("id is not a number")
+    }
+    if(!user){
+        return response.send("not found")
+    }
+
+    response.json(user)
+})
